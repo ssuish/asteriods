91c56be03a7601a44b8651d2e79186f6f27a8ac0
ssuish <quidor.adrean@gmail.com>
2026-01-20 10:52:28 +0800

    Add player rotation and update functionality, incorporating player turn speed constant

--- main.py
+++ main.py
@@ -13,21 +13,22 @@ def main():
     print(f"Screen height: {SCREEN_HEIGHT}")
 
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
     
     while True:
         log_state()
-        player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
 
         screen.fill("black")
 
-        for event in pygame.event.get():
-            
-            player.draw(screen)
+        player.draw(screen)
 
+        for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
 
-            dt = clock.tick(60) / 1000
+        dt = clock.tick(60) / 1000
+
+        player.update(dt)
         
         pygame.display.flip()
 
--- player.py
+++ player.py
@@ -1,7 +1,7 @@
 from time import pthread_getcpuclockid
 import pygame
 from circleshape import CircleShape
-from constants import PLAYER_RADIUS, LINE_WIDTH
+from constants import PLAYER_RADIUS, LINE_WIDTH, PLAYER_TURN_SPEED
 
 class Player(CircleShape):
     def __init__(self, x, y):
@@ -19,3 +19,14 @@ class Player(CircleShape):
 
     def draw(self, screen):
         pygame.draw.polygon(screen, "white", self.triangle(), LINE_WIDTH)
+
+    def rotate(self, angle):
+        self.rotation += angle
+
+    def update(self, dt):
+        keys = pygame.key.get_pressed()
+
+        if keys[pygame.K_a]:
+            self.rotate(-PLAYER_TURN_SPEED * dt)
+        if keys[pygame.K_d]:
+            self.rotate(PLAYER_TURN_SPEED * dt)
